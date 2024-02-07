2a4115f696cb4a0b7593b20f37cd4a8da0fbfb3f
lifestream <ruslan.inanov.35@mail.ru>
2024-02-08 01:38:23 +0300

    add product page

--- config/web.php
+++ config/web.php
@@ -19,6 +19,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'cxhKRF7zfeIEyxPf9p2ie7MYg0GN10G6',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
--- models/Product.php
+++ models/Product.php
@@ -11,4 +11,9 @@ class Product extends ActiveRecord
 			return 'product';
 		}
 
+		public function getCategory()
+		{
+			return $this->hasOne(Category::class, ['id' => 'category_id']);
+		}
+
 }
